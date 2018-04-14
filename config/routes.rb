3ab02703bc703ed6a53b6dd79d36f5e42ac363ac
Alexander Merkulov <sasha@merqlove.ru>
2018-04-14 19:00:11 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Spree::Core::Engine.routes.draw do
   devise_for :spree_user,
              :class_name => 'Spree::User',
-             :controllers => { :sessions => 'spree/user_sessions',
+             :controllers => { :omniauth_callbacks => "spree/omniauth_callbacks",
+                               :sessions => 'spree/user_sessions',
                                :registrations => 'spree/user_registrations',
                                :passwords => 'spree/user_passwords' },
-             :skip => [:unlocks, :omniauth_callbacks],
+             :skip => [:unlocks],
              :path_names => { :sign_out => 'logout' },
-             :path_prefix => :user
+             :path => 'users'
 
   resources :users, :only => [:edit, :update]
 
@@ -20,6 +21,8 @@ Spree::Core::Engine.routes.draw do
     post '/password/recover' => 'user_passwords#create', :as => :reset_password
     get '/password/change' => 'user_passwords#edit', :as => :edit_password
     put '/password/change' => 'user_passwords#update', :as => :update_password
+    
+    post "/users/sign_up_social", to: "user_registrations#create_social", as: "social_user_registration"
   end
 
   match '/checkout/registration' => 'checkout#registration', :via => :get, :as => :checkout_registration
